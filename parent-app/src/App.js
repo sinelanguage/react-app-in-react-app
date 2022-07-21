6218f993aa30d9c40ac462fc5a28855d1f6de516
Adam Winick <sinelanguage@users.noreply.github.com>
2022-07-20 23:28:06 -0400

    Update App.js

--- parent-app/src/App.js
+++ parent-app/src/App.js
@@ -7,8 +7,8 @@ import { broadcastChannelDefaults } from './constants';
 const {parent, channel} = broadcastChannelDefaults
 
 function App() {
-  const broadCastChannel = new BroadcastChannel(channel.name);
-  const ldpChannelRef = useRef(broadCastChannel);
+  const broadcastChannel = new BroadcastChannel(channel.name);
+  const ldpChannelRef = useRef(broadcastChannel);
   const ldpChannel = ldpChannelRef.current;
   const dispatch = useDispatch();
 
